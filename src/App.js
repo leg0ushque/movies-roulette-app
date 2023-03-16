99d484fdc838f35b6e33f9f221bda598592eee5a
Vadzim Zemchonak <vadzim_zemchonak@epam.com>
2023-03-17 02:29:03 +0300

    Create Counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Counter } from "./Components/Counter/index";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>movies-roulette</h1>
+        <Counter value="321"></Counter>
       </header>
     </div>
   );
