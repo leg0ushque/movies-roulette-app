f105a9ff817086efa18469793f6d608d3e0d907d
Vadzim Zemchonak <vadzim_zemchonak@epam.com>
2023-03-18 15:14:45 +0300

    Create SearchForm component

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class SearchForm extends Component {
+  state = {
+    defaultValue: '',
+    onSearch: (value) => {},
+  };
+
+  constructor(props) {
+    super(props);
+    this.state.defaultValue = props.defaultValue;
+    this.state.onSearch = props.onSearch;
+  }
+
+  search = () => {
+    let searchInput = document.getElementById('searchInput');
+    this.state.onSearch(searchInput.value)
+  }
+
+  render() {
+    return (
+      <>
+        <div className="search-form">
+          <input type="text" placeholder="What do you want to watch?"
+            id="searchInput" className="search-input" 
+            defaultValue={this.state.defaultValue}></input>
+          <button type="button" className="search-button" onClick={this.search}>SEARCH</button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default SearchForm;
