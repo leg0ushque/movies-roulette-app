199e1d5e57dd8e523ce5eca19af3f65fc5d22fc3
Vadzim Zemchonak <vadzim_zemchonak@epam.com>
2023-03-19 17:55:56 +0300

    Update SearchForm component to be controlled

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,38 +2,50 @@ import React, { Component } from "react";
 import "./styles.css";
 
 const DEFAULT_VALUE = "";
+const INPUT_PLACEHOLDER_TEXT = "What do you want to watch?";
+const INPUT_SUBMIT_TEXT = "SEARCH";
 
 class SearchForm extends Component {
   state = {
-    defaultValue: DEFAULT_VALUE,
+    value: DEFAULT_VALUE,
     onSearch: (value) => {},
   };
 
   constructor(props) {
     super(props);
-    this.state.defaultValue = props.initialValue || DEFAULT_VALUE;
+    this.state.value = props.initialValue || DEFAULT_VALUE;
     this.state.onSearch = props.onSearch;
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  search = () => {
-    let searchInput = document.getElementById("searchInput");
-    this.state.onSearch(searchInput.value);
-  };
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.state.onSearch(this.state.value);
+    this.setState((state) => {
+      return {
+        value: "",
+        onChange: state.onChange,
+      };
+    });
+  }
 
   render() {
     return (
-      <div className="search-form">
+      <form onSubmit={this.handleSubmit} className="search">
         <input
           type="text"
-          placeholder="What do you want to watch?"
-          id="searchInput"
-          className="search-input"
-          defaultValue={this.state.defaultValue}
+          placeholder={INPUT_PLACEHOLDER_TEXT}
+          value={this.state.value}
+          onChange={this.handleChange}
         ></input>
-        <button type="button" className="search-button" onClick={this.search}>
-          SEARCH
-        </button>
-      </div>
+        <input type="submit" value={INPUT_SUBMIT_TEXT} />
+      </form>
     );
   }
 }
