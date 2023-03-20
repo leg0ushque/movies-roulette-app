3adf86c22d63de0ee5e4b531e0d4f9671eb0910e
VZ <118164566+leg0ushque@users.noreply.github.com>
2023-03-20 13:32:54 +0300

    Module 2 HW (#2)
    * Create an app with create-react-app
    
    * Clear CRA
    
    * Create Counter component
    
    * Create SearchForm component
    
    * Create GenreSelect component
    
    * Update App.js to use all the components
    
    * Comments resolve

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class Counter extends Component {
+  state = {
+    value: 0,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state.value = props.initialValue;
+  }
+
+  incrementCounter = () => {
+    this.setState((state) => {
+      return { value: +state.value + 1 };
+    });
+  };
+
+  decrementCounter = () => {
+    this.setState((state) => {
+      return { value: +state.value - 1 };
+    });
+  };
+
+  render() {
+    return React.createElement("div", { className: "counter" }, [
+      React.createElement(
+        "span",
+        { className: "counterValue" },
+        this.state.value
+      ),
+      React.createElement("div", {}, [
+        React.createElement(
+          "button",
+          {
+            className: "button incButton",
+            type: "button",
+            onClick: this.incrementCounter,
+          },
+          "+1"
+        ),
+        React.createElement(
+          "button",
+          {
+            className: "button decButton",
+            type: "button",
+            onClick: this.decrementCounter,
+          },
+          "-1"
+        ),
+      ]),
+    ]);
+  }
+}
+
+export default Counter;
