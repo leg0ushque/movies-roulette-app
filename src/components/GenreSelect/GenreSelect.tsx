6373c3c429f8552799b3e0872149d09e577dbdd0
VZ <118164566+leg0ushque@users.noreply.github.com>
2023-03-22 13:45:23 +0300

    Migration to TypeScript (#3)
    * Full migration of created components
    
    * Use Eslint

---
+++ src/components/GenreSelect/GenreSelect.tsx
@@ -0,0 +1,41 @@
+import './styles.css';
+
+import React from 'react';
+
+import type Genre from '../../shared/types/IGenre';
+
+export interface IGenreSelectProps {
+  genres: Genre[]
+  selectedGenreId: string
+  onSelect: (genreId: string) => void
+}
+
+class GenreSelect extends React.Component<IGenreSelectProps> {
+  select (genreId: string): void {
+    if (genreId.length > 0) {
+      this.props.onSelect(genreId);
+    }
+  };
+
+  createGenreButton (genre: Genre, isSelected: boolean): JSX.Element {
+    return (
+            <li
+                key={genre.id}
+                className={isSelected ? 'selected' : ''}
+                onClick={() => { this.select(genre.id); }}
+            >
+                {genre.name}
+            </li>
+    );
+  }
+
+  render (): JSX.Element {
+    const genresButtons = this.props.genres.map((genre: Genre) =>
+      this.createGenreButton(genre,
+        this.props.selectedGenreId === genre.id));
+
+    return <ul className="genresList">{genresButtons}</ul>;
+  }
+}
+
+export default GenreSelect;
---
+++ src/components/SearchForm/SearchForm.tsx
@@ -0,0 +1,69 @@
+import './styles.css';
+
+import React, { type ChangeEvent, type FormEvent } from 'react';
+
+const DEFAULT_VALUE = '';
+const INPUT_PLACEHOLDER_TEXT = 'What do you want to watch?';
+const INPUT_SUBMIT_TEXT = 'SEARCH';
+
+interface ISearchFormProps {
+  initialValue: string
+  onSearch: (value: string) => void
+}
+
+interface ISearchFormState {
+  value: string
+  onSearch: (value: string) => void
+}
+
+class SearchForm extends React.Component<ISearchFormProps, ISearchFormState> {
+  state = {
+    value: DEFAULT_VALUE,
+    onSearch: (value: string) => {}
+  };
+
+  defaultProps = {
+    value: DEFAULT_VALUE
+  }
+
+  constructor (props: ISearchFormProps) {
+    super(props);
+    this.state.value = props.initialValue;
+    this.state.onSearch = props.onSearch;
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange (event: ChangeEvent<HTMLInputElement>): void {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit (event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    this.state.onSearch(this.state.value);
+    this.setState((state) => {
+      return {
+        value: '',
+        onSearch: state.onSearch
+      };
+    });
+  }
+
+  render (): JSX.Element {
+    return (
+      <form onSubmit={this.handleSubmit} className="search">
+        <input
+          type="text"
+          placeholder={INPUT_PLACEHOLDER_TEXT}
+          value={this.state.value}
+          onChange={this.handleChange}
+          required
+        ></input>
+        <input type="submit" value={INPUT_SUBMIT_TEXT} />
+      </form>
+    );
+  }
+}
+
+export default SearchForm;
