d3d3f0582119d914e11bf96d938fee8042ab92aa
Vadzim Zemchonak <vadzim_zemchonak@epam.com>
2023-04-02 13:55:14 +0300

    Fix GenreSelect import

--- src/stories/GenreSelectWrapper.tsx
+++ src/stories/GenreSelectWrapper.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 
-import GenreSelect from '../components/GenreSelect/';
+import GenreSelect from '../components/GenreSelect';
 
 import type Genre from '../shared/types/IGenre';
 
