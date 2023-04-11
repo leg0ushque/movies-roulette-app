cb27be093617fd4e7b3dbf62dcead7397e746d33
Vadzim Zemchonak <vadzim_zemchonak@epam.com>
2023-04-12 01:31:40 +0300

    Add focus-trapping using react-focus-lock
    Remove focus-trap-react

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,7 +1,8 @@
-import { Portal } from 'react-portal';
 import './styles.css';
 
 import React from 'react';
+import FocusLock from 'react-focus-lock';
+import { Portal } from 'react-portal';
 
 export interface IDialogProps {
   logo: string
@@ -12,36 +13,38 @@ export interface IDialogProps {
 }
 
 const logo = (content: string | null): JSX.Element | null => {
-  if (content !== null && content.length > 0) {
-    return (
-      <div className="dialog-logo" role="dialog-logo">
-        {content}
-      </div>
-    )
-  } else {
+  if (content == null || content.length === 0) {
     return null
   }
+
+  return (
+    <div className="dialog-logo" role="dialog-logo">
+      {content}
+    </div>
+  )
 }
 
 const Dialog: React.FC<React.PropsWithChildren<IDialogProps>> = (props) => {
   return (
     <Portal>
-      <div className="dialog-overlay" />
-      <div className={
-        'dialog' +
-        (props.isNotification ? ' notification' : '') +
-        (props.isCentered ? ' centered' : '')} role="dialog">
-        <div className="dialog-header">
-            <button className="dialog-close-button" role="dialog-close-button" onClick={props.onClose}>&#10005;</button>
-        </div>
-        {logo(props.logo)}
-        <div className="dialog-title" role="dialog-title">
-          {props.title}
+        <FocusLock>
+          <div className="dialog-overlay" />
+          <div className={
+            'dialog' +
+            (props.isNotification ? ' notification' : '') +
+            (props.isCentered ? ' centered' : '')} role="dialog">
+            <div className="dialog-header">
+                <button className="dialog-close-button" role="dialog-close-button" onClick={props.onClose}>&#10005;</button>
+            </div>
+            {logo(props.logo)}
+            <div className="dialog-title" role="dialog-title">
+              {props.title}
+              </div>
+            <div className="dialog-body" role="dialog-body">
+              {props.children}
+            </div>
           </div>
-        <div className="dialog-body" role="dialog-body">
-          {props.children}
-        </div>
-      </div>
+      </FocusLock>
     </Portal>
   );
 }
--- src/stories/DialogWrapper.tsx
+++ src/stories/DialogWrapper.tsx
@@ -1,5 +1,4 @@
 
-import FocusTrap from 'focus-trap-react';
 import React, { useState } from 'react';
 
 import Dialog from '../components/Dialog';
@@ -22,20 +21,18 @@ const DialogWrapper: React.FC<React.PropsWithChildren<IDialogWrapperProps>> = (p
     <>
     <button className="redFocusedButton" onClick={openDialog} >Open dialog</button>
     { dialogIsOpened &&
-      <FocusTrap active={dialogIsOpened}>
-        <Dialog
-          logo={props.logo}
-          title={props.title}
-          isNotification={props.isNotification}
-          isCentered={props.isCentered}
-          onClose={() => {
-            closeDialog();
-            props.onClose();
-          }}
-        >
-          {props.children}
-        </Dialog>
-      </FocusTrap>
+      <Dialog
+        logo={props.logo}
+        title={props.title}
+        isNotification={props.isNotification}
+        isCentered={props.isCentered}
+        onClose={() => {
+          closeDialog();
+          props.onClose();
+        }}
+      >
+        {props.children}
+      </Dialog>
     }
     </>
   );
